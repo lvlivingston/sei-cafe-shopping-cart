835a8537f7e74f579aefb6f2b9cb86f6113468c3
lvlivingston <lvlivingston@gmail.com>
2023-11-14 14:11:35 -0600

    updated order model

--- models/order.js
+++ models/order.js
@@ -48,4 +48,18 @@ orderSchema.virtual('orderId').get(function () {
     return this.id.slice(-6).toUpperCase();
 });
 
+// statics are callable on the model, not an instance (document)
+orderSchema.statics.getCart = function(userId) {
+    // 'this' is bound to the model (don't use an arrow function)
+    // return the promise that resolves to a cart (the user's unpaid order)
+    return this.findOneAndUpdate(
+      // query object
+      { user: userId, isPaid: false },
+      // update document - provides values when inserting new order... in the case the order (cart) is upserted
+      { user: userId },
+      // options object... upsert option creates the doc if it doesn't exist!
+      { upsert: true, new: true }
+    );
+};
+
 module.exports = mongoose.model('Order', orderSchema);
