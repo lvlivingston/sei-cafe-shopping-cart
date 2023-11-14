ea9fb5aadb2e0255904a38818de38bfc2cfc60b9
lvlivingston <lvlivingston@gmail.com>
2023-11-14 13:52:55 -0600

    defined order model

--- models/order.js
+++ models/order.js
@@ -3,8 +3,19 @@ const Schema = mongoose.Schema;
 const itemSchema = require('./itemSchema');
 
 const lineItemSchema = new Schema({
+    // Set qty to 1 when new item pushed into lineItems
     qty: { type: Number, default: 1 },
     item: itemSchema
+}, {
+    timestamps: true,
+    // Add this to ensure virtuals are serialized
+    toJSON: { virtuals: true }
+});
+
+// Add an extPrice to the line item
+lineItemSchema.virtual('extPrice').get(function () {
+// the 'this' keyword is bound to the lineItem subdocument
+return this.qty * this.item.price;
 });
 
 const orderSchema = new Schema({
@@ -19,7 +30,22 @@ const orderSchema = new Schema({
     // A user's unpaid order is their "cart"
     isPaid: { type: Boolean, default: false },
 }, {
-  timestamps: true
+    timestamps: true,
+    // Serialize those virtuals!
+    toJSON: { virtuals: true }
+});
+
+// Add the following helpful virtuals to order documents
+orderSchema.virtual('orderTotal').get(function () {
+    return this.lineItems.reduce((total, item) => total + item.extPrice, 0);
+});
+
+orderSchema.virtual('totalQty').get(function () {
+    return this.lineItems.reduce((total, item) => total + item.qty, 0);
+});
+
+orderSchema.virtual('orderId').get(function () {
+    return this.id.slice(-6).toUpperCase();
 });
 
 module.exports = mongoose.model('Order', orderSchema);
